a8b1626814dbe5b51dc5de642886b15e5cce8f94
Dung Bui Viet <dbuiviet.xcomp@gmail.com>
2024-10-02 14:05:05 +0000

    add simple-mern-project files

---
+++ frontend/src/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+
+import Header from './components/Header/Header';
+import NewProduct from './components/Products/NewProduct';
+import ProductList from './components/Products/ProductList';
+import './App.css';
+
+function App() {
+  const [loadedProducts, setLoadedProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsLoading(true);
+      const response = await fetch('https://5000-dbuivietxcom-mernwebdev-zgrggmu1ft9.ws-us116.gitpod.io/products', {credentials: "omit"});
+
+      const responseData = await response.json();
+
+      setLoadedProducts(responseData.products);
+      setIsLoading(false);
+    };
+
+    fetchProducts();
+  }, []);
+
+  const addProductHandler = async (productName, productPrice) => {
+    try {
+      const newProduct = {
+        title: productName,
+        price: +productPrice // "+" to convert string to number
+      };
+      let hasError = false;
+      const response = await fetch('https://5000-dbuivietxcom-mernwebdev-zgrggmu1ft9.ws-us116.gitpod.io/product', {
+        method: 'POST',
+        body: JSON.stringify(newProduct),
+        headers: {
+          'Content-Type': 'application/json',
+          //'Access-Control-Allow-Origin': '*'
+          'Access-Control-Allow-Origin': 'https://5000-dbuivietxcom-mernwebdev-zgrggmu1ft9.ws-us116.gitpod.io'
+        },
+        credentials: "omit"
+      });
+
+      if (!response.ok) {
+        hasError = true;
+      }
+
+      const responseData = await response.json();
+
+      if (hasError) {
+        throw new Error(responseData.message);
+      }
+
+      setLoadedProducts(prevProducts => {
+        return prevProducts.concat({
+          ...newProduct,
+          id: responseData.product.id
+        });
+      });
+    } catch (error) {
+      alert(error.message || 'Something went wrong!');
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <NewProduct onAddProduct={addProductHandler} />
+        {isLoading && <p className="loader">Loading...</p>}
+        {!isLoading && <ProductList items={loadedProducts} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
